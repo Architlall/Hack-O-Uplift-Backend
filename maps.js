9c67ff2d6406713d8a2e31a68b1b62e293f72b13
Architlall <79276124+Architlall@users.noreply.github.com>
2021-09-04 02:47:04 +0530

    added live location tracking

--- maps.js
+++ maps.js
@@ -1,22 +1,38 @@
-var myLatLng = { lat: 38.3460, lng: -0.4907 };
-var mapOptions = {
+x= navigator.geolocation;
+
+ x.getCurrentPosition(success,failure);
+ var directionsDisplay;
+ var directionsService;
+ 
+ function success(position){
+     var myLat = position.coords.latitude;
+
+    var myLng = position.coords.longitude;
+    var myLatLng = { lat: myLat, lng: myLng };
+    var mapOptions = {
     center: myLatLng,
     zoom: 7,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 
 };
 
-//create map
-var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions); // for existing googleMap div in html file
 
-//create a DirectionsService object to use the route method and get a result for our request
-var directionsService = new google.maps.DirectionsService();
+var map = new google.maps.Map(document.getElementById('map'), mapOptions); // for existing map div in html fil 
+   
+     var marker = new google.maps.Marker({map: map, position: myLatLng});
+    //create a DirectionsService object to use the route method and get a result for our request
+  directionsService = new google.maps.DirectionsService();
 
-//create a DirectionsRenderer object which we will use to display the route
-var directionsDisplay = new google.maps.DirectionsRenderer();
+ //create a DirectionsRenderer object which we will use to display the route
+  directionsDisplay = new google.maps.DirectionsRenderer();
 
 //bind the DirectionsRenderer to the map
 directionsDisplay.setMap(map);
+
+        
+ }
+
+function failure(){}
 
 
 //define calcRoute function
@@ -62,12 +78,12 @@ function calcRoute() {
 
 
 //create autocomplete objects for all inputs
-var options = {
-    types: ['(cities)']
-}
+// var options = {
+//     types: ['(cities)']
+// }
 
-var input1 = document.getElementById("from");
-var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+// var input1 = document.getElementById("from");
+// var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
 
-var input2 = document.getElementById("to");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+// var input2 = document.getElementById("to");
+// var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
