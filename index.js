4c343196625bace5d39d609bad6690da98f6a916
Shaviaditya <rudrashiv311@gmail.com>
2021-09-05 04:11:55 +0530

    Trying to deploy

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const methodOverride = require('method-override')
 const router = require('./routes/requestRoutes');
 const userrouter = require('./routes/user-route');
 const donorrouter = require('./routes/donorRoutes')
-
+const path = require("path")
 require('dotenv').config();
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "Hack-O-Uplift-Frontend", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Hack-O-Uplift-Frontend", "public", "index.html"));
+});
 const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ydfmd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(URI,{useNewUrlParser: true, useUnifiedTopology: true})
  .then((result)=> 
